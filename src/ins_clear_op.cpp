68374f83118d41e03bbe0d7ce38accd7961d773a
Jason P. Leasure <jpleasu@gmail.com>
2020-10-08 09:27:23 -0400

    consolidate clear hooks

--- src/ins_clear_op.cpp
+++ src/ins_clear_op.cpp
@@ -1,116 +1,58 @@
 #include "ins_clear_op.h"
 #include "ins_helper.h"
 
-/* threads context */
-extern thread_ctx_t *threads_ctx;
+namespace {
+    struct clear_instrumentation : public instrumentation_base<clear_instrumentation> {
+        template <char code0, size_t sz>
+        static void HOOK_DECL unary(THREADID tid, typename Tagset<code0>::arg_type op0) {
+            Tagset<code0> tags0(tid, op0);
+            for (size_t i = 0; i < sz; ++i)
+                tags0.set(i, tag_traits<tag_t>::cleared_val);
+            tags0.template zext<sz>();
+        }
+    };
+} // namespace
 
 static void PIN_FAST_ANALYSIS_CALL r_clrl4(THREADID tid) {
-  for (size_t i = 0; i < 8; i++) {
-    RTAG[DFT_REG_RDX][i] = tag_traits<tag_t>::cleared_val;
-    RTAG[DFT_REG_RCX][i] = tag_traits<tag_t>::cleared_val;
-    RTAG[DFT_REG_RBX][i] = tag_traits<tag_t>::cleared_val;
-    RTAG[DFT_REG_RAX][i] = tag_traits<tag_t>::cleared_val;
-  }
+    for (size_t i = 0; i < 8; i++) {
+        RTAG[DFT_REG_RDX][i] = tag_traits<tag_t>::cleared_val;
+        RTAG[DFT_REG_RCX][i] = tag_traits<tag_t>::cleared_val;
+        RTAG[DFT_REG_RBX][i] = tag_traits<tag_t>::cleared_val;
+        RTAG[DFT_REG_RAX][i] = tag_traits<tag_t>::cleared_val;
+    }
 }
 
 static void PIN_FAST_ANALYSIS_CALL r_clrl2(THREADID tid) {
-  for (size_t i = 0; i < 8; i++) {
-    RTAG[DFT_REG_RDX][i] = tag_traits<tag_t>::cleared_val;
-    RTAG[DFT_REG_RAX][i] = tag_traits<tag_t>::cleared_val;
-  }
-}
-
-static void PIN_FAST_ANALYSIS_CALL r_clrb_l(THREADID tid, uint32_t reg) {
-  RTAG[reg][0] = tag_traits<tag_t>::cleared_val;
-}
-
-static void PIN_FAST_ANALYSIS_CALL r_clrb_u(THREADID tid, uint32_t reg) {
-  RTAG[reg][1] = tag_traits<tag_t>::cleared_val;
-}
-
-static void PIN_FAST_ANALYSIS_CALL r_clrw(THREADID tid, uint32_t reg) {
-  for (size_t i = 0; i < 2; i++) {
-    RTAG[reg][i] = tag_traits<tag_t>::cleared_val;
-  }
-}
-
-static void PIN_FAST_ANALYSIS_CALL r_clrl(THREADID tid, uint32_t reg) {
-  // assume zero extension to full register
-  for (size_t i = 0; i < 8; i++) {
-    RTAG[reg][i] = tag_traits<tag_t>::cleared_val;
-  }
-}
-
-static void PIN_FAST_ANALYSIS_CALL r_clrq(THREADID tid, uint32_t reg) {
-  for (size_t i = 0; i < 8; i++) {
-    RTAG[reg][i] = tag_traits<tag_t>::cleared_val;
-  }
-}
-
-static void PIN_FAST_ANALYSIS_CALL r_clrx(THREADID tid, uint32_t reg) {
-  for (size_t i = 0; i < 16; i++) {
-    RTAG[reg][i] = tag_traits<tag_t>::cleared_val;
-  }
-}
-
-static void PIN_FAST_ANALYSIS_CALL r_clry(THREADID tid, uint32_t reg) {
-  for (size_t i = 0; i < 16; i++) {
-    RTAG[reg][i] = tag_traits<tag_t>::cleared_val;
-  }
+    for (size_t i = 0; i < 8; i++) {
+        RTAG[DFT_REG_RDX][i] = tag_traits<tag_t>::cleared_val;
+        RTAG[DFT_REG_RAX][i] = tag_traits<tag_t>::cleared_val;
+    }
 }
 
 void ins_clear_op(INS ins) {
-  if (INS_OperandIsMemory(ins, OP_0)) {
-    INT32 n = INS_OperandWidth(ins, OP_0) / 8;
-    M_CLEAR_N(n);
-  } else {
-    REG reg_dst = INS_OperandReg(ins, OP_0);
-    if (REG_is_gr64(reg_dst)) {
-      R_CALL(r_clrq, reg_dst);
-    } else if (REG_is_gr32(reg_dst)) {
-      R_CALL(r_clrl, reg_dst);
-    } else if (REG_is_gr16(reg_dst)) {
-      R_CALL(r_clrw, reg_dst);
-    } else if (REG_is_xmm(reg_dst)) {
-      R_CALL(r_clrx, reg_dst);
-    } else if (REG_is_mm(reg_dst)) {
-      R_CALL(r_clrq, reg_dst);
-    } else if (REG_is_ymm(reg_dst)) {
-      R_CALL(r_clry, reg_dst);
-    } else {
-      if (REG_is_Upper8(reg_dst))
-        R_CALL(r_clrb_u, reg_dst);
-      else
-        R_CALL(r_clrb_l, reg_dst);
-    }
-  }
+    clear_instrumentation::ins_unary_op(ins);
 }
 
 void ins_clear_op_predicated(INS ins) {
-  // one byte
-  if (INS_MemoryOperandCount(ins) == 0) {
-    REG reg_dst = INS_OperandReg(ins, OP_0);
+    // one byte
+    if (INS_MemoryOperandCount(ins) == 0) {
+        REG reg_dst = INS_OperandReg(ins, OP_0);
 
-    if (REG_is_Upper8(reg_dst))
-      INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)r_clrb_u,
-                               IARG_FAST_ANALYSIS_CALL, IARG_THREAD_ID,
-                               IARG_UINT32, REG_INDX(reg_dst), IARG_END);
-    else
-      INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)r_clrb_l,
-                               IARG_FAST_ANALYSIS_CALL, IARG_THREAD_ID,
-                               IARG_UINT32, REG_INDX(reg_dst), IARG_END);
-  } else
-    INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)tagmap_clrn,
-                             IARG_FAST_ANALYSIS_CALL, IARG_MEMORYWRITE_EA,
-                             IARG_UINT32, 1, IARG_END);
+        if (REG_is_Upper8(reg_dst))
+            INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)clear_instrumentation::unary<'R', 1>,
+                                     IARG_FAST_ANALYSIS_CALL, IARG_THREAD_ID, IARG_UINT32, REG_INDX(reg_dst), IARG_END);
+        else
+            INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)clear_instrumentation::unary<'r', 1>,
+                                     IARG_FAST_ANALYSIS_CALL, IARG_THREAD_ID, IARG_UINT32, REG_INDX(reg_dst), IARG_END);
+    } else
+        INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)clear_instrumentation::unary<'m', 1>,
+                                 IARG_FAST_ANALYSIS_CALL, IARG_THREAD_ID, IARG_MEMORYWRITE_EA, IARG_END);
 }
 
 void ins_clear_op_l2(INS ins) {
-  INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)r_clrl2, IARG_FAST_ANALYSIS_CALL,
-                 IARG_THREAD_ID, IARG_END);
+    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)r_clrl2, IARG_FAST_ANALYSIS_CALL, IARG_THREAD_ID, IARG_END);
 }
 
 void ins_clear_op_l4(INS ins) {
-  INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)r_clrl4, IARG_FAST_ANALYSIS_CALL,
-                 IARG_THREAD_ID, IARG_END);
+    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)r_clrl4, IARG_FAST_ANALYSIS_CALL, IARG_THREAD_ID, IARG_END);
 }
